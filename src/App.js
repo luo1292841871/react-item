164258a4805d346d2b7911eb80c7ce3f5113f3f8
ljm <1123212@qq.com>
2019-08-13 19:28:27 +0800

    搭建路由

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './pages/login';
 class App extends Component{
   render() {
     return <Router>
+      {/*只要以"/"开头的，都会匹配和加载 */}
       <Route path="/" component={Admin}/>
       <Route path="/login" component={Login}/>
     </Router>
